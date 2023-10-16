3aa2aec20a3a676acbd26f7b2d3b96a81b9aae1a
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-16 17:18:17 -0300

    Feat: Instalação do Roter e Style-components

---
+++ src/components/views/Header/Header.Style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyleHeader = styled.header`
+border: solid black;
+background-color: black;
+
+.listaLinks{
+    display: flex;
+   justify-content: space-around;
+
+}
+`
---
+++ src/pages/Filmes/Filmes.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Filmes = () => {
+  return (
+    <div>Filmes</div>
+  )
+}
+
+export default Filmes
