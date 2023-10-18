64c8e8edbba3c3c17d498ac92fbc3406de71a4a7
LarissaSchmidt <larissaschmidt541@gmail.com>
2023-10-17 23:20:42 -0300

    feat: Eslitização de componente

--- src/components/views/Home/BannerCine1/BannerCine1.jsx
+++ src/components/views/Home/BannerCine1/BannerCine1.jsx
@@ -1,7 +1,7 @@
 import { StyleBannerCine1 } from './Bannercine1.style'
 import Button from '../../../common/Button/Button'
 import MinhaImagen from '../../../../../public/filme1.png'
-import  {Alarm} from "@phosphor-icons/react";
+import { Alarm } from "@phosphor-icons/react";
 import { tema } from '../../../../style/theme';
 import Filme2 from '../../../../../public/filme2.png'
 import Filme3 from '../../../../../public/Filme3.png'
@@ -10,22 +10,22 @@ import figurapromo from '../../../../../public/figurapromocao.png'
 import Figurameia from '../../../../../public/figurapagameia.png'
 const Bannercine1 = () => {
 
-  return (
-    <StyleBannerCine1>
+    return (
+        <StyleBannerCine1>
             <section className='initBox'>
-            <div className='buttonBox'>    
-            <h1>Cine Samurai Curitiba</h1>
-            <Button texto='Programação' variant='primary' />
-            <Button texto='Preços' variant='primary'/>
-            </div>
-            <h2>Em Breve</h2>
+                <div className='buttonBox'>
+                    <h1>Cine Samurai Curitiba</h1>
+                    <Button texto='Programação' variant='primary' />
+                    <Button texto='Preços' variant='primary' />
+                </div>
+                <h2>Em Breve</h2>
             </section>
             <section className='posterBox'>
-                <Poster 
-                imagem={MinhaImagen} 
-                color={tema.white50} 
-                titulo='A Freira 2' 
-                sinopse={`A Freira 2 é o segundo capítulo da história de A Freira (2018), 
+                <Poster
+                    imagem={MinhaImagen}
+                    color={tema.white50}
+                    titulo='A Freira 2'
+                    sinopse={`A Freira 2 é o segundo capítulo da história de A Freira (2018), 
                 que faz parte do universo da franquia Invocação do Mal. No primeiro filme, 
                 após uma freira cometer suicídio em um convento na Romênia, 
                 o Vaticano envia o atormentado Padre Burke (Demián Bichir) e uma noviça,
@@ -36,54 +36,54 @@ const Bannercine1 = () => {
                  Agora, na continuação, anos após os acontecimentos do primeiro filme, um padre é assassinado e parece que o mal está se espalhando por toda a região. 
                  Novamente acompanhamos a Irmã Irene quando, após pensar ter escapado por pouco de Valak, a entidade demoníaca, ela é forçada a enfrentar o poderoso e macabro inimigo mais uma vez.
                 `}
-                
-            />
-            <Poster
-                imagem={Filme2}
-                color={tema.white50}
-                titulo='O Exorcista'
-                sinopse={`O Exorcista - O Devoto é a sequência do clássico de 1973 sobre uma menina de 12 anos que é possuída por uma misteriosa entidade demoníaca, 
+
+                />
+                <Poster
+                    imagem={Filme2}
+                    color={tema.white50}
+                    titulo='O Exorcista'
+                    sinopse={`O Exorcista - O Devoto é a sequência do clássico de 1973 sobre uma menina de 12 anos que é possuída por uma misteriosa entidade demoníaca, 
                 forçando sua mãe a buscar a ajuda de dois padres para salvá-la. Nesta versão de 2023,
                  desde a morte de sua esposa grávida em um terremoto no Haiti, 
                  há 12 anos, Victor Fielding tem criado sua filha Angela sozinho. 
                  Mas quando Angela e sua amiga Katherine desaparecem na floresta e retornam três dias depois sem memória do que aconteceu com elas, 
                  isso desencadeia uma série de eventos que obrigará Victor a confrontar o mal e, 
                  em seu terror e desespero, buscar a única pessoa viva que testemunhou algo parecido antes: Chris MacNeil. Baseado no best-seller de William Peter Blatty.`}
-                
-            />
-            <Poster
-                imagem={Filme3}
-                color={tema.white50}
-                titulo='Ruim Pra Cachorro'
-                sinopse={`Ruim pra Cachorro é um filme norte-americano de comédia dirigido por Josh Greenbaum que acompanha a história de Reggie (Will Ferrell),
+
+                />
+                <Poster
+                    imagem={Filme3}
+                    color={tema.white50}
+                    titulo='Ruim Pra Cachorro'
+                    sinopse={`Ruim pra Cachorro é um filme norte-americano de comédia dirigido por Josh Greenbaum que acompanha a história de Reggie (Will Ferrell),
                  um inocente e adorável cachorro abandonado nas ruas por Doug (Will Forte), 
                  seu antigo dono. Em sua nova e triste vida, os caminhos do animal e de uma gangue de vira-latas liderada por um Boston Terrier acabam se cruzando,
                   e o grupo começa uma improvável amizade. Na verdade, ainda vão mais além: os cães se unem para levar Reggie de volta pra casa para fazer Doug pagar pelo que fez. Com Sofía Vergara, 
                   Jamie Foxx, Isla Fisher e Josh Gad.`}
-                
-            />
-            
+
+                />
+
             </section>
             <section className='cardPromocao'>
-             <picture><img src={figurapromo} alt="" /></picture>  
-             <div className='primeiroBloco'>
-             <h2>Quem Paga</h2>
-             <picture ><img src={Figurameia} alt="" /></picture>
-             <h2>?</h2>
-             </div> 
-             <div className='segundoBloco'>
-            <ul>
-                <li>Estudantes</li>
-                <li>Idosos</li>
-                <li>Professores</li>
-                <li>Portadores de deficiência</li>
-                <li>Doador de sangue</li>
-            </ul>
-             </div>
+                <picture><img src={figurapromo} alt="" /></picture>
+                <div className='primeiroBloco'>
+                    <h2>Quem Paga</h2>
+                    <picture ><img src={Figurameia} alt="" /></picture>
+                    <h2>?</h2>
+                </div>
+                <div className='segundoBloco'>
+                    <ul>
+                        <li>Estudantes</li>
+                        <li>Idosos</li>
+                        <li>Professores</li>
+                        <li>Portadores de deficiência</li>
+                        <li>Doador de sangue</li>
+                    </ul>
+                </div>
             </section>
-            
-    </StyleBannerCine1>
-  )
+
+        </StyleBannerCine1>
+    )
 }
 
 
