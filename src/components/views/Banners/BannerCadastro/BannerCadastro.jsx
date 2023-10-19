3626b10965e5214de7c2cda1320464e110f7dbb2
ericafernandaj <eriicafernandaj@gmail.com>
2023-10-19 20:19:29 -0300

    Feat: Estilização Banner Cadastro

--- src/components/views/Banners/BannerCadastro/BannerCadastro.jsx
+++ src/components/views/Banners/BannerCadastro/BannerCadastro.jsx
@@ -12,24 +12,32 @@ const handleClick = () => {
   return (
 
     < StyleCadastro>
+    <section>
     <h1>Cadastre-se</h1>
+    </section>
+    <section className="section-input">
+
     <section>
     <Input text ='Nome' placeholder='Nome'/>
     <Input text ='Sobrenome' placeholder='Sobrenome'/>
     <Input text ='Email' placeholder='Email'/>
     <Input text ='CPF' placeholder='CPF'/>
-    <Input text ='Telefone' placeholder='Telefone'/>
-    <Input text ='Logradouro' placeholder='Logradouro'/>
+    <Input text ='Telefone' placeholder='Telefone'/>   
+    <Input text ='Logradouro' placeholder='Logradouro'/>    
+    </section> 
+    <section>
     <Input text ='Numero' placeholder='Numero'/>
     <Input text ='Complemento' placeholder='Complemento'/>
     <Input text ='Cidade' placeholder='Cidade'/>
     <Input text ='UF' placeholder='UF'/>
-    <Input text ='CEP' placeholder='CEP'/>
-    
+    <Input text ='CEP' placeholder='CEP'/>    
     </section>
+    </section>
+
     <section>  
         <Button texto ='Cadastrar' variant='primary' onClick={handleClick}/>
     </section>
+    
 
     </ StyleCadastro>
   )
--- src/components/views/Banners/BannerCadastro/BannerCadatro.style.js
+++ src/components/views/Banners/BannerCadastro/BannerCadatro.style.js
@@ -1,12 +1,62 @@
 import styled from "styled-components";
 
 export const StyleCadastro = styled.div`
-margin: 20px;
 display: flex;
-Flex-direction: column;
+flex-direction: column;
 align-items: center;
 border: solid white;
 white: 50%;
-height: 100%;
+width: 70%;
+margin: 30px auto;
+background-color: #E9F0F0;
+border-radius: 8px;
 
+
+
+h1 {
+    font-family: 'Roboto',sans-serif;
+    font-weight: 500;
+    text-decoration: none;
+    font-size: 1.7rem;
+    align-items: center;
+    margin-top: 20px;
+    
+    
+}
+
+input {
+    margin-left: 20px;
+    border-radius: 8px;
+    padding: 12px 15px;
+    width: 300px;
+    font-weight: 650;
+    line-height: normal;
+    font-size: 1rem;
+    font-family: 'Roboto',sans-serif;
+    margin-bottom: 20px;
+    space-around: 10px;
+}
+
+section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 20px;
+    background-color: #E9F0F0;
+} 
+
+
+.div-principal {
+    display: flex;
+    border-radius: 12px;
+    background-color: #E9F0F0;
+    width: 50%;
+    padding: 90px;
+}
+
+.section-input{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
 `
