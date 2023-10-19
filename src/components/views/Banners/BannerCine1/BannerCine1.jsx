e4e4053957753638a001b93df85f67c5f70890e5
ericafernandaj <eriicafernandaj@gmail.com>
2023-10-19 00:40:05 -0300

    Feat: Criação de componentes

--- src/components/views/Banners/BannerCine1/BannerCine1.jsx
+++ src/components/views/Banners/BannerCine1/BannerCine1.jsx
@@ -1,38 +1,38 @@
-import { StyleBannerCine1 } from './BannerCine1.style';
-import Button from '../../../common/Button/Button';
-import ImagemFreira from '../../../../../public/filme1.png';
-import { tema } from '../../../../style/theme';
-import Filme2 from '../../../../../public/filme2.png';
-import Filme3 from '../../../../../public/filmeCachorro.jpg'
-import Poster from '../../Poster/Poster';
-import figurapromo from '../../../../../public/figurapromocao.png';
-import Figurameia from '../../../../../public/meia.svg';
-import FaixaEtaria from "../../../../../public/faixaEtaria.svg";
-import { useNavigate } from 'react-router-dom';
+import { StyleBannerCine1 } from "./BannerCine1.style";
+import Button from "../../../common/Button/Button";
+import ImagemFreira from "../../../../../public/filme1.png";
+import { tema } from "../../../../style/theme";
+import Filme2 from "../../../../../public/filme2.png";
+import Filme3 from "../../../../../public/filmeCachorro.jpg";
+import Poster from "../../Poster/Poster";
+import figurapromo from "../../../../../public/figurapromocao.png";
+import Figurameia from "../../../../../public/meia.svg";
+import FaixaEtaria from "../../../../../public/FaixaEtaria.svg";
+import { useNavigate } from "react-router-dom";
 const Bannercine1 = () => {
-    const navigate = useNavigate();
-const handleClick = () => {
-    navigate('/Filmes');
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/Filmes");
   };
   const handlePreco = () => {
-      navigate('/Precos');
-    };
-    return (
-        <StyleBannerCine1>
-            <section className='initBox'>
-                <div className='buttonBox'>
-                    <h1>🍿 Cine Samurai Curitiba</h1>
-                    <Button texto='Programação' variant='primary' onClick={handleClick}/>
-                    <Button texto='Preços' variant='primary' onClick={handlePreco}/>
-                </div>
-                <h2>Em Breve</h2>
-            </section>
-            <section className='posterBox'>
-                <Poster
-                    imagem={ImagemFreira}
-                    color={tema.white50}
-                    titulo='A Freira 2'
-                    sinopse={`A Freira 2 é o segundo capítulo da história de A Freira (2018), 
+    navigate("/Precos");
+  };
+  return (
+    <StyleBannerCine1>
+      <section className="initBox">
+        <div className="buttonBox">
+          <h1>🍿 Cine Samurai Curitiba</h1>
+          <Button texto="Programação" variant="primary" onClick={handleClick} />
+          <Button texto="Preços" variant="primary" onClick={handlePreco} />
+        </div>
+        <h2>Em Breve</h2>
+      </section>
+      <section className="posterBox">
+        <Poster
+          imagem={ImagemFreira}
+          color={tema.white50}
+          titulo="A Freira 2"
+          sinopse={`A Freira 2 é o segundo capítulo da história de A Freira (2018), 
                 que faz parte do universo da franquia Invocação do Mal. No primeiro filme, 
                 após uma freira cometer suicídio em um convento na Romênia, 
                 o Vaticano envia o atormentado Padre Burke (Demián Bichir) e uma noviça,
@@ -43,66 +43,57 @@ const handleClick = () => {
                  Agora, na continuação, anos após os acontecimentos do primeiro filme, um padre é assassinado e parece que o mal está se espalhando por toda a região. 
                  Novamente acompanhamos a Irmã Irene quando, após pensar ter escapado por pouco de Valak, a entidade demoníaca, ela é forçada a enfrentar o poderoso e macabro inimigo mais uma vez.
                 `}
-                    faixa={FaixaEtaria}
-
-                />
-                <Poster
-                    imagem={Filme2}
-                    color={tema.white50}
-                    titulo='O Exorcista'
-                    sinopse={`O Exorcista - O Devoto é a sequência do clássico de 1973 sobre uma menina de 12 anos que é possuída por uma misteriosa entidade demoníaca, 
+          faixa={FaixaEtaria}
+        />
+        <Poster
+          imagem={Filme2}
+          color={tema.white50}
+          titulo="O Exorcista"
+          sinopse={`O Exorcista - O Devoto é a sequência do clássico de 1973 sobre uma menina de 12 anos que é possuída por uma misteriosa entidade demoníaca, 
                 forçando sua mãe a buscar a ajuda de dois padres para salvá-la. Nesta versão de 2023,
                  desde a morte de sua esposa grávida em um terremoto no Haiti, 
                  há 12 anos, Victor Fielding tem criado sua filha Angela sozinho. 
                  Mas quando Angela e sua amiga Katherine desaparecem na floresta e retornam três dias depois sem memória do que aconteceu com elas, 
                  isso desencadeia uma série de eventos que obrigará Victor a confrontar o mal e, 
                  em seu terror e desespero, buscar a única pessoa viva que testemunhou algo parecido antes: Chris MacNeil. Baseado no best-seller de William Peter Blatty.`}
-                 faixa={FaixaEtaria}
-
-                />
-                <Poster
-                    imagem={Filme3}
-                    color={tema.white50}
-                    titulo='Ruim Pra Cachorro'
-                    sinopse={`Ruim pra Cachorro é um filme norte-americano de comédia dirigido por Josh Greenbaum que acompanha a história de Reggie (Will Ferrell),
+          faixa={FaixaEtaria}
+        />
+        <Poster
+          imagem={Filme3}
+          color={tema.white50}
+          titulo="Ruim Pra Cachorro"
+          sinopse={`Ruim pra Cachorro é um filme norte-americano de comédia dirigido por Josh Greenbaum que acompanha a história de Reggie (Will Ferrell),
                  um inocente e adorável cachorro abandonado nas ruas por Doug (Will Forte), 
                  seu antigo dono. Em sua nova e triste vida, os caminhos do animal e de uma gangue de vira-latas liderada por um Boston Terrier acabam se cruzando,
                   e o grupo começa uma improvável amizade. Na verdade, ainda vão mais além: os cães se unem para levar Reggie de volta pra casa para fazer Doug pagar pelo que fez. Com Sofía Vergara, 
                   Jamie Foxx, Isla Fisher e Josh Gad.`}
-                 faixa={FaixaEtaria}
+          faixa={FaixaEtaria}
+        />
+      </section>
+      <section className="cardPromocao">
+        <picture>
+          <img className="pipocaMeia" src={figurapromo} alt="" />
+        </picture>
 
+        <div className="primeiroBloco">
+          <h2 className="tituloBloco">Quem Paga</h2>
+          <picture>
+            <img className="meia" src={Figurameia} alt="" />
+          </picture>
+          <h2>?</h2>
+        </div>
+        <div className="segundoBloco">
+          <ul>
+            <li>Estudantes</li>
+            <li>Idosos</li>
+            <li>Professores</li>
+            <li>Portadores de deficiência</li>
+            <li>Doador de sangue</li>
+          </ul>
+        </div>
+      </section>
+    </StyleBannerCine1>
+  );
+};
 
-                />
-
-            </section>
-            <section className='cardPromocao'>
-                <picture><img className="pipocaMeia" src={figurapromo} alt="" /></picture>
-                
-
-                    <div className='primeiroBloco'>
-                        <h2 className="tituloBloco">Quem Paga</h2>
-                        <picture ><img className="meia" src={Figurameia} alt="" /></picture>
-                        <h2>?</h2>
-                    </div>
-                    <div className='segundoBloco'>
-                        <ul>
-                            <li>Estudantes</li>
-                            <li>Idosos</li>
-                            <li>Professores</li>
-                            <li>Portadores de deficiência</li>
-                            <li>Doador de sangue</li>
-                        </ul>
-                    </div>
-                
-            </section>
-
-        </StyleBannerCine1>
-    )
-}
-
-
-
-
-
-
-export default Bannercine1
+export default Bannercine1;
