54b1c0ee237f57c494204899aa9c66dd93e531fb
laisamarcelino <laisamsr12@gmail.com>
2023-10-20 00:29:28 -0300

    refactor: correcao do nome das cores

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,16 +1,15 @@
 export const tema = {
     blue900: '#263238',
-    yellow: '#d266ff',
+    magenta: '#d266ff',
     blue500: '#101728',
     blue200: '#A8DADC',
     blue100: '#D0E8E8',
     white50: '#FFFFFF',
     white200: '#E9F0F0',
-    red700: '#A3151A',
-    red500: '#9A4A4D',
-    red50: '#FF3C5F',
+    crimson700: '#A3151A',
+    crimson500: '#9A4A4D',
+    crimson50: '#FF3C5F',
     black700: '#000000',
     green500: '#34673F',
-    green50: '#E8F3EB',
-  }
-  
+    green50: '#E8F3EB'
+}
