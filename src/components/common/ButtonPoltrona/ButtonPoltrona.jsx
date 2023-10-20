a809846ff2f52cd8352de8d1503ae3acdaba6514
Lucas Rolando <lucasrls98@gmail.com>
2023-10-19 23:16:47 -0300

    style: estilizando poltronas

--- src/components/common/ButtonPoltrona/ButtonPoltrona.jsx
+++ src/components/common/ButtonPoltrona/ButtonPoltrona.jsx
@@ -1,10 +1,10 @@
 import styled from "styled-components"
-const ButtonPoltrona = ({numero , onClick, selecionado, id}) => {
+const ButtonPoltrona = ({ numero, onClick, selecionado, id }) => {
   return (
 
     <StylePoltrona onClick={() => onClick(id)}>
-        {numero}
-    
+      {numero}
+
     </StylePoltrona>
   )
 }
@@ -12,13 +12,23 @@ const ButtonPoltrona = ({numero , onClick, selecionado, id}) => {
 export default ButtonPoltrona
 
 const StylePoltrona = styled.div`
-border-radius:50%;
+/* border-radius:50%;
 width: 35px;
 height: 35px;
 background-color: white;
 display: flex;
 justify-content: center;
 align-items: center;
-color: black;
+color: black; */
+display: flex;
+justify-content:center;
+align-items: center;
+background-color: #444451;
+height: 35px;
+width: 35px;
+margin: 10px;
+border-top-left-radius: 29px;
+border-top-right-radius: 29px;
 cursor: pointer;
+
 `
