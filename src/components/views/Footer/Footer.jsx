f60b4242e6353d39ebeddcf932879d3f28279cb8
laisamarcelino <laisamsr12@gmail.com>
2023-10-20 21:42:33 -0300

    style: insercao de margin top no style

--- src/components/views/Footer/Footer.jsx
+++ src/components/views/Footer/Footer.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
-import { StyleFooter } from './Footer.style'
-import  {MapPin,LinkedinLogo,InstagramLogo, FacebookLogo} from "@phosphor-icons/react";
-import { tema } from '../../../style/theme';
+import React from "react";
+import { StyleFooter } from "./Footer.style";
+import {
+  MapPin,
+  LinkedinLogo,
+  InstagramLogo,
+  FacebookLogo,
+} from "@phosphor-icons/react";
+import { tema } from "../../../style/theme";
 const Footer = () => {
   return (
-  <StyleFooter>
-    <div className='rodape'>
-      
-    <section className='localização-icone'>
-        <MapPin className='map' color={tema.white50} size={20}weight="fill" />
-        <p className='nome-rua-rodape'>Rua Qualquer, 123, Cidade QualquerCuritiba-PR</p>
-    </section>
-      
-<section className='icones'>
-<a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-<LinkedinLogo color={tema.white50} size={40} weight="fill" />
-</a>
-<a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-<InstagramLogo color={tema.white50}size={40}/>
-</a>
-<a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-<FacebookLogo color={tema.white50} size={40} />
-</a>
-</section>
-<section className='contato'>
-    <h3>Fale conosco</h3>
-    <p>(41) 3456-7890</p>
-<p>ola@grandesite.com.br</p>    
-</section>
-</div>
-    
-  </StyleFooter>
-  )
+    <StyleFooter>
+      <div className="rodape">
+        <section className="localização-icone">
+          <MapPin
+            className="map"
+            color={tema.white50}
+            size={20}
+            weight="fill"
+          />
+          <p className="nome-rua-rodape">
+            Rua Qualquer, 123, Cidade QualquerCuritiba-PR
+          </p>
+        </section>
+
+        <section className="icones">
+          <a
+            href="https://www.linkedin.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <LinkedinLogo color={tema.white50} size={40} weight="fill" />
+          </a>
+          <a
+            href="https://www.instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <InstagramLogo color={tema.white50} size={40} />
+          </a>
+          <a
+            href="https://www.facebook.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FacebookLogo color={tema.white50} size={40} />
+          </a>
+        </section>
+        <section className="contato">
+          <h3>Fale conosco</h3>
+          <p>(41) 3456-7890</p>
+          <p>ola@grandesite.com.br</p>
+        </section>
+      </div>
+    </StyleFooter>
+  );
 };
 
-export default Footer
+export default Footer;
--- src/components/views/Footer/Footer.style.js
+++ src/components/views/Footer/Footer.style.js
@@ -1,52 +1,48 @@
 import styled from "styled-components";
 
-export const StyleFooter = styled.footer `
-background-color:rgb(27,27,27);
-height: 20vh;
-color:rgb(247,247,247);
-display: flex;
+export const StyleFooter = styled.footer`
+  background-color: rgb(27, 27, 27);
+  height: 20vh;
+  color: rgb(247, 247, 247);
+  display: flex;
+  margin-top: 50px;
 
-.rodape{
-display:flex;
-flex-direction: row;
-align-items:center;
-justify-content: space-around;
-width: 100%;
-}
-.localização-icone{
-display: flex;
-align-items: center;
+  .rodape {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+  }
+  .localização-icone {
+    display: flex;
+    align-items: center;
+  }
+  .nome-rua-rodape {
+    font-family: "Montserrat", sans-serif;
+  }
+  .map {
+    margin-right: 4px;
+  }
+  .contato {
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+  }
+  .contato h3,
+  .contato p {
+    margin: auto;
+    font-size: 16px;
+    font-family: "Montserrat", sans-serif;
+  }
 
-}
-.nome-rua-rodape{
-font-family: 'Montserrat', sans-serif;
-
-}
-.map{
-margin-right: 4px;
-}
-.contato{
-display: flex;
-flex-direction: column;
-gap: 0; 
-}
-.contato h3, .contato p {
-margin: auto;
-font-size: 16px; 
-font-family: 'Montserrat', sans-serif;
-}
-
-.icones svg{
+  .icones svg {
     cursor: pointer;
-    margin-right:10px;
-}
-.icones svg:hover{
+    margin-right: 10px;
+  }
+  .icones svg:hover {
     transform: scale(1.2);
-    transition: .3s;
-    opacity: .5;
-}
-
-
-
-
-`
+    transition: 0.3s;
+    opacity: 0.5;
+  }
+`;
