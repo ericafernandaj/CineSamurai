d373e96e552fb8bc1183db60d0dece8f288b1bb4
laisamarcelino <laisamsr12@gmail.com>
2023-10-24 15:54:46 -0300

    refactor: correcao das requisicoes login e cadastro

--- src/service/api.js
+++ src/service/api.js
@@ -1,32 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000', 
+  baseURL: "http://localhost:3000",
 });
 
 export const loginUsuario = async (email, senha) => {
-    try {
-        // const config = {
-        //     headers: {
-        //         'X-password': senha,
-        //     },
-        // };
+  try {
+    // const config = {
+    //     headers: {
+    //         'X-password': senha,
+    //     },
+    // };
 
-        const resposta = await api.post('/auth/login', { email:email, senha:senha }); 
-
-        return resposta.data;
-    } catch (error) {
-        if (error.response) {
-            return {
-                message: error.response.data.message,
-                success: error.response.data.success,
-            };
-        } else {
-            return {
-                message: 'erro inesperado',
-            };
-        }
+    const resposta = await api.post("/auth/login", { email, senha });
+    console.log(resposta);
+    return resposta.data;
+  } catch (error) {
+    if (error.response) {
+      return {
+        message: error.response.message,
+      };
+    } else {
+      return {
+        message: "erro inesperado",
+      };
     }
+  }
 };
 
 export const postUsuario = async (body, senha) => {
@@ -35,6 +34,10 @@ export const postUsuario = async (body, senha) => {
   //     'X-password': senha,
   //   },
   // }
-  const resposta = await api.post('/usuarios', body, senha)
-  return resposta.data
-}
+  try {
+    const resposta = await api.post("/usuarios", { ...body, senha });
+    return resposta.data;
+  } catch (error) {
+    console.log(error.response);
+  }
+};
