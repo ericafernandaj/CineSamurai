00bb5ac494c14eed50739aef93f90445c3909344
laisamarcelino <laisamsr12@gmail.com>
2023-10-24 15:53:40 -0300

    update: correcao da conexao handleCadastro com o o back-end

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -3,35 +3,75 @@ import Footer from "../../components/views/Footer/Footer";
 import { StyleCadastro } from "./cadastro.style";
 import Textfield from "../../components/views/Textfield/Textfield";
 import Button from "../../components/common/Button/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { postUsuario } from "../../service/api";
+import { cpf } from "cpf-cnpj-validator";
 
 const Cadastro = () => {
+  const navigate = useNavigate();
   const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmaSenha, setConfirmaSenha] = useState("");
-  const [cpf, setCPF] = useState("");
+  const [cpf_usuario, setCPF] = useState("");
   const [telefone, setTelefone] = useState("");
 
+  function validaNome(nome) {
+    return typeof nome == "string" && nome.length > 2;
+  }
+
+  function validaEmail(email) {
+    return (
+      typeof email == "string" &&
+      email.length > 11 &&
+      email.includes("@") &&
+      email.includes(".com") &&
+      (email.includes("gmail") || email.includes("hotmail"))
+    );
+  }
+
+  function validaCPF(cpf_usuario) {
+    return cpf.isValid(cpf_usuario);
+  }
+
+  function validaTelefone(telefone) {
+    let telefoneInt;
+    try {
+      telefoneInt = parseInt(telefone);
+    } catch {
+      return false;
+    }
+    return (
+      typeof telefone == "string" &&
+      telefone.length == 11 &&
+      telefone == telefoneInt
+    );
+  }
+
+  function validaSenha(senha, confirmaSenha){
+    return senha.length > 7 && senha.length <= 12 && senha == confirmaSenha
+}
+
   const handleCadastro = async (e) => {
+
     e.preventDefault();
 
     const body = {
       nome,
       email,
-      cpf,
+      cpf: cpf_usuario, //cpf AS cpf_usuario
       telefone,
     };
 
-    if (senha === confirmaSenha) {
+    if (validaNome && validaEmail && validaSenha && validaCPF && validaTelefone) {
+      navigate("/Login")
       const resposta = await postUsuario(body, senha);
-      localStorage.setItem("id", resposta.data.id);
-      localStorage.setItem("nome", resposta.data.nome);
       console.log(resposta);
+      localStorage.setItem("id", resposta.id_usuario); 
+      
     } else {
-      console.log("As senhas precisam ser iguais");
+      console.log("Verifique as informações");
     }
   };
 
@@ -41,82 +81,81 @@ const Cadastro = () => {
       <StyleCadastro>
         <section>
           <h1>Cadastre-se</h1>
-        
 
-        <form>
-          <div className="align-textfield">
-            <Textfield
-              label="Nome:"
-              name="Nome"
-              type="text"
-              placeholder="Nome"
-              required
-              value={nome}
-              onChange={(e) => setNome(e)}
+          <form>
+            <div className="align-textfield">
+              <Textfield
+                label="Nome:"
+                name="Nome:"
+                type="text"
+                placeholder="Nome"
+                required
+                value={nome}
+                onChange={(e) => setNome(e)}
+              />
+              <Textfield
+                label="Email:"
+                name="Email:"
+                type="email"
+                placeholder="email@email.com"
+                required
+                value={email}
+                onChange={(e) => setEmail(e)}
+              />
+            </div>
+
+            <div className="align-textfield">
+              <Textfield
+                label="CPF:"
+                name="CPF:"
+                type="text"
+                placeholder="xxxxxxxxxxx"
+                required
+                value={cpf_usuario}
+                onChange={(e) => setCPF(e)}
+              />
+              <Textfield
+                label="Telefone:"
+                name="Telefone:"
+                placeholder="xxxxxxxxxxx"
+                required
+                value={telefone}
+                onChange={(e) => setTelefone(e)}
+              />
+            </div>
+
+            <div className="align-textfield">
+              <Textfield
+                label="Senha:"
+                name="Senha:"
+                placeholder="●●●●●●●"
+                type="password"
+                required
+                value={senha}
+                onChange={(e) => setSenha(e)}
+              />
+              <Textfield
+                label="Confirme a senha:"
+                name="Confirme a senha:"
+                placeholder="●●●●●●●"
+                type="password"
+                required
+                value={confirmaSenha}
+                onChange={(e) => setConfirmaSenha(e)}
+              />
+            </div>
+          </form>
+
+          <div className="div-cadastro">
+            <Button
+              texto="Cadastrar"
+              variant="primary"
+              onClick={handleCadastro}
             />
-            <Textfield
-              label="Email:"
-              name="email"
-              type="email"
-              placeholder="email@email.com"
-              required
-              value={email}
-              onChange={(e) => setEmail(e)}
-            />
+            <Link to="/login" className="link-login">
+              Já possuo cadastro.
+            </Link>
           </div>
-
-          <div className="align-textfield">
-            <Textfield
-              label="CPF:"
-              name="cpf"
-              type="text"
-              placeholder="xxxxxxxxxxx"
-              required
-              value={cpf}
-              onChange={(e) => setCPF(e)}
-            />
-            <Textfield
-              label="Telefone:"
-              name="telefone"
-              placeholder="xxxxxxxxxxx"
-              required
-              value={telefone}
-              onChange={(e) => setTelefone(e)}
-            />
-          </div>
-
-          <div className="align-textfield">
-            <Textfield
-              label="Senha:"
-              name="senha"
-              placeholder="●●●●●●●"
-              type="password"
-              required
-              value={senha}
-              onChange={(e) => setSenha(e)}
-            />
-            <Textfield
-              label="Confirme a senha:"
-              name="confirmaSenha"
-              placeholder="●●●●●●●"
-              type="password"
-              required
-              value={confirmaSenha}
-              onChange={(e) => setConfirmaSenha(e)}
-            />
-          </div>
-        </form>
-
-        <div className="div-cadastro">
-          <Button
-            texto="Cadastrar"
-            variant="primary"
-            onClick={handleCadastro}
-            />
-          <Link to="/login" className="link-login">
-            Já possuo cadastro.
-          </Link>
-        </div>
         </section>
       </StyleCadastro>
       <Footer />
