8ed6602519da55555db41bece4737ca1d7c08f21
laisamarcelino <laisamsr12@gmail.com>
2023-10-25 16:59:33 -0300

    style: correcao de estilo da pag

--- src/components/views/Banners/BannerQuemSomos/BannerQuemSomos.style.js
+++ src/components/views/Banners/BannerQuemSomos/BannerQuemSomos.style.js
@@ -6,35 +6,33 @@ export const StyleQuemSomos = styled.div`
     flex-direction: column;
     align-items: center;
     border: solid white;
-    white: 80%;
     width: 70%;
     margin: 80px auto;
     border: none;
     color: #f2f2f2;
-    font-family: "Work Sans", sans-serif;
+    font-family: "Roboto", sans-serif;
   }
 
-  p {
-    font-size: 1.5rem;
+  div {
     flex-direction: column;
     align-items: center;
-    white: 80%;
-    width: 70%;
     margin: 20px auto;
     border-radius: 4px;
     border: none;
     padding: 10px;
-    text-aligner: left;
+    text-align: justify;
     line-height: 27px;
+    font-family: "Roboto", sans-serif;
   }
-  h1,
-  h2 {
+
+  h1, h2 {
     font-family: "Roboto", sans-serif;
     font-weight: 500;
     text-decoration: none;
     font-size: 2rem;
     color: #fabd2b;
   }
+
   h3 {
     font-size: 1rem;
     color: #fabd2b;
