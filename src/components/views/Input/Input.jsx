44d78bd29db631b510eb2641a3144236904c4868
laisamarcelino <laisamsr12@gmail.com>
2023-10-25 16:59:54 -0300

    fix: correcao de input

--- src/components/views/Input/Input.jsx
+++ src/components/views/Input/Input.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { StyleInput } from "./input.styles"
 import { tema } from "../../../style/theme";
 
-const Input = ({ placeholder, hasError, isValid, required, name, type, value, onChange }) => {
+const Input = ({ placeholder, hasError, isValid, required, name, type, value, onChange, id }) => {
   const variantes = {
     error: `2px solid ${tema.crimson700}`,
     valid: `2px solid ${tema.green500}`,
@@ -13,6 +13,7 @@ const Input = ({ placeholder, hasError, isValid, required, name, type, value, on
       name={name}
       type={type}
       value={value}
+      id={id}
       onChange={(e) => onChange(e.target.value)}
       style={{
         border: isValid ? variantes.valid : hasError ? variantes.error : "",
