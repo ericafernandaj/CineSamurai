c6081a4292f6491f7c89bc2913b9b53c5a18f170
laisamarcelino <laisamsr12@gmail.com>
2023-10-27 14:38:54 -0300

    style: adicao de espaçamento entre botoes

--- src/pages/AreaUsuario/areaUsuario.style.js
+++ src/pages/AreaUsuario/areaUsuario.style.js
@@ -4,9 +4,8 @@ export const StyleAreaUsuario = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
-  margin-top: 35px;
-  margin-bottom: 10%;
+  gap: 30px;
+  margin: 5% auto;
 
 
   form {
