4e20da5bdd638273d8b3d08661d85208ca2dbbd1
laisamarcelino <laisamsr12@gmail.com>
2023-10-27 14:38:02 -0300

    style: adicao de tela

--- src/components/views/SelecionarPoltrona/SelecionarPoltrona.jsx
+++ src/components/views/SelecionarPoltrona/SelecionarPoltrona.jsx
@@ -3,7 +3,7 @@ import ButtonPoltrona from "../../common/ButtonPoltrona/ButtonPoltrona"
 import { useEffect, useState } from "react"
 const SelecionarPoltrona = () => {
     const linha = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const colunas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    const colunas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
     const [poltronasSelecionadas, setPoltranasSelecionadas] = useState([])
 
     useEffect(() => {
@@ -18,15 +18,21 @@ const SelecionarPoltrona = () => {
     }
     return (
         <StylePoltrona>
+<section>
 
             {
                 colunas.map((letra) => {
                     return (
-                        <ul>
+                        <ul key={letra}>
+                        <p className="corLetras">{letra}</p>
                             {linha.map((numero) => <ButtonPoltrona id={letra + numero} numero={numero} onClick={handleSelecionarPoltrona} poltronasSelecionadas={poltronasSelecionadas} setPoltronasSelecionadas={setPoltranasSelecionadas} />)}
                         </ul>)
                 })
             }
+            <div className="tela">
+                <p>Tela</p>
+            </div>
+            </section>
 
         </StylePoltrona>
     )
--- src/components/views/SelecionarPoltrona/SelecionarPoltrona.style.js
+++ src/components/views/SelecionarPoltrona/SelecionarPoltrona.style.js
@@ -1,22 +1,46 @@
 import styled from "styled-components";
 
 export const StylePoltrona = styled.div`
-margin: 0;
-ul{
+  margin: 0;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  ul {
     display: flex;
     align-items: center;
-    justify-content: center;    
+    justify-content: center;
     gap: 6px;
+  }
 
-}
-
-ul div:hover{
+  ul div:hover {
     background-color: #f54c5a;
+  }
 
-}
-
-ul div:active{
+  ul div:active {
     background-color: #f54c5a;
-}
+  }
 
-`
+  .corLetras {
+    color: #E9F0F0;
+  }
+
+  .tela {
+    margin-top: 30px;
+    width: 80%;
+    height: 40px;
+    border-radius: 8px;
+    background-color: #E9F0F0;
+    color: #101728;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Montserrat',sans-serif;
+    font-size: 1rem;
+    font-weight: 700;
+    line-height: normal;
+  }
+`;
